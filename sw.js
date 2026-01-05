786b4da9e39f63ea88e1e0048fdab878456c8dde
pzylan123 <pzylan00@gmail.com>
2026-01-06 00:24:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.json() : { message: 'New message in Zychat!' };
+    const data = event.data ? event.data.json() : { content: 'New Message!' };
     
+    const options = {
+        body: data.content,
+        icon: '/icon.png', // Add a 192x192 icon to your GitHub
+        badge: '/icon.png',
+        vibrate: [100, 50, 100],
+        data: { url: 'https://pzylan123.github.io/Zychat/' },
+        tag: 'zychat-new-msg', // Groups messages together
+        renotify: true
+    };
+
     event.waitUntil(
-        self.registration.showNotification('Zychat', {
-            body: data.message,
-            icon: '/icon.png',
-            vibrate: [200, 100, 200],
-            tag: 'zychat-notif',
-            renotify: true
-        })
+        self.registration.showNotification('ZyChat Zenith', options)
     );
 });
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(
-        clients.openWindow('/')
-    );
+    event.waitUntil(clients.openWindow(event.notification.data.url));
 });
